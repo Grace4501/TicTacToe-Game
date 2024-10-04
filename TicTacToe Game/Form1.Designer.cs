c75e3bbf7a5987a8fbdc92abe9b07877107f9d30
janairorobert7 <pasionrobert7@gmail.com>
2024-10-04 12:08:02 -0700

    Finished the board

--- TicTacToe Game/Form1.Designer.cs
+++ TicTacToe Game/Form1.Designer.cs
@@ -28,8 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             PlayerWinCount = new Label();
             CPUWinCount = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            RestartGameBtn = new Button();
             SuspendLayout();
             // 
             // PlayerWinCount
@@ -37,7 +50,7 @@
             PlayerWinCount.AutoSize = true;
             PlayerWinCount.Font = new Font("Summer Dream Sans Demo", 10.2F, FontStyle.Italic, GraphicsUnit.Point, 0);
             PlayerWinCount.ForeColor = Color.LimeGreen;
-            PlayerWinCount.Location = new Point(54, 138);
+            PlayerWinCount.Location = new Point(69, 148);
             PlayerWinCount.Name = "PlayerWinCount";
             PlayerWinCount.Size = new Size(138, 26);
             PlayerWinCount.TabIndex = 0;
@@ -48,21 +61,159 @@
             CPUWinCount.AutoSize = true;
             CPUWinCount.Font = new Font("Summer Dream Sans Demo", 10.8F, FontStyle.Italic, GraphicsUnit.Point, 0);
             CPUWinCount.ForeColor = Color.Red;
-            CPUWinCount.Location = new Point(492, 137);
+            CPUWinCount.Location = new Point(507, 147);
             CPUWinCount.Name = "CPUWinCount";
             CPUWinCount.Size = new Size(104, 27);
             CPUWinCount.TabIndex = 1;
             CPUWinCount.Text = "CPU Win:";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(130, 189);
+            button1.Name = "button1";
+            button1.Size = new Size(139, 132);
+            button1.TabIndex = 2;
+            button1.Text = "?";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(275, 189);
+            button2.Name = "button2";
+            button2.Size = new Size(139, 132);
+            button2.TabIndex = 3;
+            button2.Text = "?";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(420, 189);
+            button3.Name = "button3";
+            button3.Size = new Size(139, 132);
+            button3.TabIndex = 4;
+            button3.Text = "?";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.Location = new Point(420, 327);
+            button4.Name = "button4";
+            button4.Size = new Size(139, 132);
+            button4.TabIndex = 7;
+            button4.Text = "?";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button5.Location = new Point(275, 327);
+            button5.Name = "button5";
+            button5.Size = new Size(139, 132);
+            button5.TabIndex = 6;
+            button5.Text = "?";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button6.Location = new Point(130, 327);
+            button6.Name = "button6";
+            button6.Size = new Size(139, 132);
+            button6.TabIndex = 5;
+            button6.Text = "?";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button7.Location = new Point(419, 465);
+            button7.Name = "button7";
+            button7.Size = new Size(139, 132);
+            button7.TabIndex = 10;
+            button7.Text = "?";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button8.Location = new Point(275, 465);
+            button8.Name = "button8";
+            button8.Size = new Size(139, 132);
+            button8.TabIndex = 9;
+            button8.Text = "?";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button9.Location = new Point(130, 465);
+            button9.Name = "button9";
+            button9.Size = new Size(139, 132);
+            button9.TabIndex = 8;
+            button9.Text = "?";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.BackgroundImage = (Image)resources.GetObject("button10.BackgroundImage");
+            button10.BackgroundImageLayout = ImageLayout.Stretch;
+            button10.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button10.Location = new Point(484, 12);
+            button10.Name = "button10";
+            button10.Size = new Size(139, 132);
+            button10.TabIndex = 11;
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.BackgroundImage = (Image)resources.GetObject("button11.BackgroundImage");
+            button11.BackgroundImageLayout = ImageLayout.Stretch;
+            button11.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button11.Location = new Point(69, 13);
+            button11.Name = "button11";
+            button11.Size = new Size(139, 132);
+            button11.TabIndex = 12;
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // RestartGameBtn
+            // 
+            RestartGameBtn.BackColor = Color.Blue;
+            RestartGameBtn.FlatStyle = FlatStyle.System;
+            RestartGameBtn.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            RestartGameBtn.ForeColor = SystemColors.ControlText;
+            RestartGameBtn.Location = new Point(275, 612);
+            RestartGameBtn.Name = "RestartGameBtn";
+            RestartGameBtn.Size = new Size(139, 53);
+            RestartGameBtn.TabIndex = 13;
+            RestartGameBtn.Text = "Restart Game";
+            RestartGameBtn.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(675, 732);
+            Controls.Add(RestartGameBtn);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button9);
+            Controls.Add(button4);
+            Controls.Add(button5);
+            Controls.Add(button6);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(CPUWinCount);
             Controls.Add(PlayerWinCount);
             Name = "Form1";
             Text = "Tic Tac Toe";
+            Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -71,5 +222,17 @@
 
         private Label PlayerWinCount;
         private Label CPUWinCount;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button RestartGameBtn;
     }
 }
--- TicTacToe Game/Form1.cs
+++ TicTacToe Game/Form1.cs
@@ -6,5 +6,10 @@ namespace TicTacToe_Game
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
