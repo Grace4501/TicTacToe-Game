89fad77bbaae6c4a90685f7beb864a6eb9a5fd19
janairorobert7 <pasionrobert7@gmail.com>
2024-10-04 11:58:09 -0700

    Starting on the board

--- TicTacToe Game/Form1.Designer.cs
+++ TicTacToe Game/Form1.Designer.cs
@@ -28,12 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            PlayerWinCount = new Label();
+            CPUWinCount = new Label();
+            SuspendLayout();
+            // 
+            // PlayerWinCount
+            // 
+            PlayerWinCount.AutoSize = true;
+            PlayerWinCount.Font = new Font("Summer Dream Sans Demo", 10.2F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            PlayerWinCount.ForeColor = Color.LimeGreen;
+            PlayerWinCount.Location = new Point(54, 138);
+            PlayerWinCount.Name = "PlayerWinCount";
+            PlayerWinCount.Size = new Size(138, 26);
+            PlayerWinCount.TabIndex = 0;
+            PlayerWinCount.Text = "Player Win:";
+            // 
+            // CPUWinCount
+            // 
+            CPUWinCount.AutoSize = true;
+            CPUWinCount.Font = new Font("Summer Dream Sans Demo", 10.8F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            CPUWinCount.ForeColor = Color.Red;
+            CPUWinCount.Location = new Point(492, 137);
+            CPUWinCount.Name = "CPUWinCount";
+            CPUWinCount.Size = new Size(104, 27);
+            CPUWinCount.TabIndex = 1;
+            CPUWinCount.Text = "CPU Win:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(675, 732);
+            Controls.Add(CPUWinCount);
+            Controls.Add(PlayerWinCount);
+            Name = "Form1";
+            Text = "Tic Tac Toe";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label PlayerWinCount;
+        private Label CPUWinCount;
     }
 }
